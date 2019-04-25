e159cd0fa5883f166d12683730ab1463270ce5de
Andre Lademann <andre.lademann@netresearch.de>
2019-04-25 11:57:44 +0200

    Add test for qos default parameter

--- spec/out.js
+++ spec/out.js
@@ -7,88 +7,93 @@ const out = require('../src/out.js')
 const baseFileDir = process.cwd() + '/spec'
 const fixtureInput = require('./fixtures/input.json')
 
-/*
-Test behavior
-
-1. Test configuration
-    - source: url, password
-    - params: message, topic
-2. Test to send messages
-3. Test callback to send status
-*/
+/**
+ * Test behavior
+ *
+ * 1. Test configuration
+ * - source: url, password
+ * - params: message, topic
+ * 2. Test to send messages
+ * 3. Test callback to send status
+ */
 describe('mqtt out', () => {
 
-    beforeEach((done) => {
-        this.input = JSON.parse(JSON.stringify(fixtureInput))
-        done()
-    })
+	beforeEach((done) => {
+		this.input = JSON.parse(JSON.stringify(fixtureInput))
+		done()
+	})
 
-    afterEach((done) => {
-        delete this.input
-        done()
-    })
+	afterEach((done) => {
+		delete this.input
+		done()
+	})
 
-    describe('source configuration', () => {
-        it('throws an error when url is missing', (done) => {
-            delete this.input.source.url
+	describe('source configuration', () => {
+		it('throws an error when url is missing', (done) => {
+			delete this.input.source.url
 
-            out(this.input, baseFileDir, (error, result) => {
-               expect(error).to.be.not.null
-                expect(error).to.exist
-                    .and.be.instanceof(Error)
-                    .and.have.property('message', 'URL for MQTT broker is not being set.')
-                done()
-            })
-        })
+			out(this.input, baseFileDir, (error, result) => {
+				expect(error).to.exist
+					.and.be.instanceof(Error)
+					.and.have.property('message', 'URL for MQTT broker is not being set.')
+				done()
+			})
+		})
 
-        it('throws an error when username is missing', (done) => {
-            delete this.input.source.username
+		it('throws an error when username is missing', (done) => {
+			delete this.input.source.username
 
-            out(this.input, baseFileDir, (error, result) => {
-                expect(error).to.be.not.null
-                expect(error).to.exist
-                    .and.be.instanceof(Error)
-                    .and.have.property('message', 'username for MQTT broker is not being set.')
-                done()
-            })
-        })
+			out(this.input, baseFileDir, (error, result) => {
+				expect(error).to.exist
+					.and.be.instanceof(Error)
+					.and.have.property('message', 'username for MQTT broker is not being set.')
+				done()
+			})
+		})
 
-        it('throws an error when password is missing', (done) => {
-            delete this.input.source.password
+		it('throws an error when password is missing', (done) => {
+			delete this.input.source.password
 
-            out(this.input, baseFileDir, (error, result) => {
-                expect(error).to.be.not.null
-                expect(error).to.exist
-                    .and.be.instanceof(Error)
-                    .and.have.property('message', 'password for MQTT broker is not being set.')
-                done()
-            })
-        })
+			out(this.input, baseFileDir, (error, result) => {
+				expect(error).to.be.not.null
+				expect(error).to.exist
+					.and.be.instanceof(Error)
+					.and.have.property('message', 'password for MQTT broker is not being set.')
+				done()
+			})
+		})
 
-        })
+	})
 
-    describe('parameter configuration', () => {
-        
-        it('throws an error when topic is missing', (done) => {
-            delete this.input.params.topic
-            out(this.input, baseFileDir, (error, result) => {
-                expect(error).to.be.not.null
-                expect(error).to.exist
-                    .and.be.instanceof(Error)
-                    .and.have.property('message', 'The parameter topic is not being set.')
-                done()
-            })
-        })
+	describe('parameter configuration', () => {
 
-        it('throws an error when message is missing', (done) => {
-            delete this.input.params.message
-            out(this.input, baseFileDir, (error, result) => {
-                expect(error).to.be.not.null
-                expect(error).to.exist
-                    .and.be.instanceof(Error)
-                    .and.have.property('message', 'The parameter message is not being set.')
-                done()
-            })
-        })
-    })
+		it('throws an error when topic is missing', (done) => {
+			delete this.input.params.topic
+			out(this.input, baseFileDir, (error, result) => {
+				expect(error).to.exist
+					.and.be.instanceof(Error)
+					.and.have.property('message', 'The parameter topic is not being set.')
+				done()
+			})
+		})
+
+		it('throws an error when message is missing', (done) => {
+			delete this.input.params.message
+			out(this.input, baseFileDir, (error, result) => {
+				expect(error).to.exist
+					.and.be.instanceof(Error)
+					.and.have.property('message', 'The parameter message is not being set.')
+				done()
+			})
+		})
+
+		it('sets default value for qos to 0', (done) => {
+			delete this.input.params.qos
+			out(this.input, baseFileDir, (error, result, params) => {
+				expect(error).to.be.null
+				expect(params.qos).to.be.equal(0)
+				done()
+			})
+		})
+	})
 })
--- src/out.js
+++ src/out.js
@@ -1,45 +1,45 @@
 'use strict'
 
 module.exports = (input, baseFileDir, callback) => {
-    const source = input.source
-    const params = input.params
-    let error = null
-    let output = null
+	const source = input.source
+	const params = input.params
+	let error = null
+	let output = null
 
-    if ( ! source.url ) {
-        if ( !error ) {
-            error = new Error('URL for MQTT broker is not being set.')
-        }
-    }
+	if ( !source.url ) {
+		if ( !error ) {
+			error = new Error('URL for MQTT broker is not being set.')
+		}
+	}
 
-    if ( ! source.username ) {
-        if ( !error ) {
-            error = new Error('username for MQTT broker is not being set.')
-        }
-    }
+	if ( !source.username ) {
+		if ( !error ) {
+			error = new Error('username for MQTT broker is not being set.')
+		}
+	}
 
-    if ( ! source.password ) {
-        if ( !error ) {
-            error = new Error('password for MQTT broker is not being set.')
-        }
-    }
+	if ( !source.password ) {
+		if ( !error ) {
+			error = new Error('password for MQTT broker is not being set.')
+		}
+	}
 
-    if ( ! params.topic ) {
-        if ( !error ) {
-            error = new Error('The parameter topic is not being set.')
-        }
-    }
+	if ( !params.topic ) {
+		if ( !error ) {
+			error = new Error('The parameter topic is not being set.')
+		}
+	}
 
-    if ( ! params.message ) {
-        if ( !error ) {
-            error = new Error('The parameter message is not being set.')
-        }
-    }
+	if ( !params.message ) {
+		if ( !error ) {
+			error = new Error('The parameter message is not being set.')
+		}
+	}
 
-    if ( ! [0, 1, 2].includes(params.qos) ) {
-        params.qos = 0
-    }
+	if ( ![ 0, 1, 2 ].includes(params.qos) ) {
+		params.qos = 0
+	}
 
-    callback(error, output)
+	callback(error, output, params)
 
 }
